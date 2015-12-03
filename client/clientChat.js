5223365568f786e75f5cd07e6039ae6d6e429846
lucia <analucia0189@gmail.com>
2015-12-03 16:50:39 -0600

    maaas

--- client/clientChat.js
+++ client/clientChat.js
@@ -1,18 +1,32 @@
-UserSession = Meteor.connection._lastSessionId;
-
 Template.chat.helpers({
-	chat: function () {
-		return Chat.find();
-	}
+    chat: function() {
+        return Chat.find({$or:[{user:Session.get("sessionId")}, {to:Session.get("sessionId")}]});
+    }
 });
 
 Template.chat.events({
-	'keypress textarea': function(ev) {
-      var $textarea = $('.input textarea')
-      if (ev.which === 13 && $textarea.val().trim() !== ''){
-        ev.stopPropagation();
-        Chat.insert({user:"User-"+UserSession, message:$textarea.val() });
-        $textarea.val('');
-      }
+    'keypress textarea': function(ev) {
+        var $textarea = $('.input textarea')
+        if (ev.which === 13 && $textarea.val().trim() !== '') {
+            //esto no deberia ir aqui pero no se donde ponerlo :(
+            if (localStorage.getItem("sessionId") === null) {
+                if (Session.equals("sessionId", undefined)) {
+                	Session.set("sessionId", Meteor.connection._lastSessionId);
+                    localStorage.setItem("sessionId", Session.get("sessionId"));
+                    Clients.insert({
+                        _id: Session.get("sessionId")
+                    });                   
+                }
+            } else {
+                Session.set("sessionId", localStorage.getItem("sessionId"));
+            }
+
+            ev.stopPropagation();
+            Chat.insert({
+                user: Session.get("sessionId").toString(),
+                message: $textarea.val()
+            });
+            $textarea.val('');
+        }
     }
 });
